4cdb2d48e108a495f3f51796f1d5cc57a60c513e
Geakie <69668715+Geakie@users.noreply.github.com>
2020-08-19 15:47:11 +0800

    Guard rendering

---
+++ SP1Framework/Guard.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Framework\console.h"
+class Guard
+
+{
+public:
+	int x, y;
+	void drawG1(Console& g_console);
+	void drawG2(Console& g_console);
+	void drawG3(Console& g_console);
+};
