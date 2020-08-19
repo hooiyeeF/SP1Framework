6db12eeba2d8ccc26907ce1b90b0943448e406dd
titanium-ii-fluoride <69669480+titanium-ii-fluoride@users.noreply.github.com>
2020-08-19 17:45:26 +0800

    fixed restart

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -245,8 +245,7 @@ void update(double dt)
             break;
         case S_ENDROOM: updateGame4();
             break;
-        case S_LOSE:
-            processUserInput();
+        case S_LOSE: processUserInput();
             PlayAgain();
             break;
     }
@@ -435,6 +434,13 @@ void PlayAgain()
 {
     if (g_skKeyEvent[K_SPACE].keyReleased)
         g_eGameState = S_SPLASHSCREEN;
+
+    reset();
+}
+void reset()
+{
+    chara.x = g_Console.getConsoleSize().X / 2 - 1;
+    chara.y = g_Console.getConsoleSize().Y / 2;
 }
 
 //--------------------------------------------------------------
