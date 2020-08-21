e72d7354b429351f9cf1fa91fbb0952464756b46
Geakie <69668715+Geakie@users.noreply.github.com>
2020-08-21 11:18:29 +0800

    stupid slnx

--- SP1Framework/game.h
+++ SP1Framework/game.h
@@ -87,7 +87,7 @@ void renderWinScreen();
 void renderMap();           // renders the map to the buffer first
 void rendertoiletpaper();   //renders the toilet paper when collected
 void renderCharacter();     // renders the character into the buffer
-void renderGuard();
+
 void renderFramerate();     // renders debug information, frame rate, elapsed time, etc
 void renderToScreen();      // dump the contents of the buffer to the screen, one frame worth of game
 void renderInputEvents();   // renders the status of input events
