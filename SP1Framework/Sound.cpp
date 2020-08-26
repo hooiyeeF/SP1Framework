578b29017f48ae1a614ecc791f2462b06c736668
Joel <Joelrts@gmail.com>
2020-08-26 15:47:31 +0800

    ONE MORE TIME

--- SP1Framework/Sound.cpp
+++ SP1Framework/Sound.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include "Sound.h"
 #include <Windows.h>
+#include "resource.h"
 
 using namespace std;
 
 void sound::BGM()
 {
-	bool music = PlaySound(L"BGM.wav", NULL, SND_FILENAME | SND_ASYNC);
+	bool music = PlaySound(MAKEINTRESOURCE(IDR_WAVE1), NULL, SND_RESOURCE | SND_ASYNC | SND_LOOP);
 }
