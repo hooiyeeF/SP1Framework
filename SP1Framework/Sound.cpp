1d3b1dde0e3ba679180d7cd191689006896d65bc
Geakie <69668715+Geakie@users.noreply.github.com>
2020-08-27 10:45:11 +0800

    Added sound to cpp

--- SP1Framework/Sound.cpp
+++ SP1Framework/Sound.cpp
@@ -8,5 +8,5 @@ using namespace std;
 
 void sound::BGM()
 {
-	bool music = PlaySound(L"../SP1Framework/Debug/Sound/Sound.wav", NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+	bool music = PlaySound(L"../SP1Framework/Sound/Sound.wav", NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
 }
